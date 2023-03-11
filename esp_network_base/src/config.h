36fe57c579b727576a5688272cee6e887695c7bf
Yoann Dufresne <yoann.dufresne0@gmail.com>
2023-03-11 14:54:35 +0100

    first esp code

---
+++ esp_network_base/src/config.h
@@ -0,0 +1,9 @@
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const char* ssid = "Your Wifi network name";
+const char* password = "Your password";
+const char* server_ip = "The IP server address";
+
+#endif
